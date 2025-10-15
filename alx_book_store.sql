bd5d3cf19be3497e016967a423c88f96434d9d9a
AureliaBett <cbettaurelia@gmail.com>
2025-10-15 04:52:46 +0300

    orders and Order Details

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,8 +21,7 @@ CREATE TABLE IF NOT EXISTS Books (
   INDEX idx_books_author_id (author_id),
   CONSTRAINT `fk_books_author`
     FOREIGN KEY (author_id) REFERENCES Authors (author_id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
+
 ) 
 
 
@@ -33,34 +32,31 @@ CREATE TABLE IF NOT EXISTS Customers (
   address TEXT,
 ) 
 
--- Orders table
-CREATE TABLE IF NOT EXISTS `Orders` (
-  `order_id` INT NOT NULL AUTO_INCREMENT,
-  `customer_id` INT NOT NULL,
-  `order_date` DATE NOT NULL,
-  PRIMARY KEY (`order_id`),
-  INDEX `idx_orders_customer_id` (`customer_id`),
-  CONSTRAINT `fk_orders_customer`
-    FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Order_Details table
-CREATE TABLE IF NOT EXISTS `Order_Details` (
-  `orderdetailid` INT NOT NULL AUTO_INCREMENT,
-  `order_id` INT NOT NULL,
-  `book_id` INT NOT NULL,
-  `quantity` DOUBLE NOT NULL DEFAULT 1,
-  PRIMARY KEY (`orderdetailid`),
+CREATE TABLE IF NOT EXISTS Orders (
+  order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  customer_id INT NOT NULL,
+  order_date DATE NOT NULL,
+   
+  INDEX `idx_orders_customer_id` (customer_id),
+  CONSTRAINT `fk_orders_customer`
+    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+
+) 
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+  orderdetailid INT NOT NULL AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  book_id INT NOT NULL,
+  quantity DOUBLE NOT NULL DEFAULT 1,
+  PRIMARY KEY (orderdetailid),
   INDEX `idx_orderdetails_order_id` (`order_id`),
   INDEX `idx_orderdetails_book_id` (`book_id`),
   CONSTRAINT `fk_orderdetails_order`
-    FOREIGN KEY (`order_id`) REFERENCES `Orders` (`order_id`)
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   CONSTRAINT `fk_orderdetails_book`
-    FOREIGN KEY (`book_id`) REFERENCES `Books` (`book_id`)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+
+) 

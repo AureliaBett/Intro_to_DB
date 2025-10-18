9ec47db0ecc17c22d02e8e1ecd194ce09b7623c8
AureliaBett <cbettaurelia@gmail.com>
2025-10-18 19:44:08 +0300

    Fixing bugs

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,12 +1,23 @@
 import mysql.connector
 
-alx_book_store = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Amchana@0!",
-    database="alx_book_store",
-    use_pure=True  
-)
-mycursor = alx_book_store.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database 'alx_book_store' created successfully!")
+try:
+        
+
+        alx_book_store = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Amchana@0!",
+            database="alx_book_store",
+            use_pure=True  
+        )
+        if alx_book_store.is_connected():
+
+            mycursor = alx_book_store.cursor()
+            mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as e:
+      print(f"Error while connecting to MySQL: {e}")
+
+finally:
+      mycursor.close()
+      alx_book_store.close()
